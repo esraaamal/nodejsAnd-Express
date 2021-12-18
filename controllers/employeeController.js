6cc7f0d878c7935ee094d34dffc20989a37cecd0
esraaamal <ma.esraa@hotmail.com>
2021-12-18 16:11:47 +0200

    b

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,5 +1,7 @@
 const express =require('express');
 var router=express.Router();
+const mongoose=require('mongoose')
+const Employee=mongoose.model('Employee');
 
 //Routes
 router.get('/',(req,res)=>{
@@ -7,4 +9,51 @@ res.render('employee/addOrEdit',{
     viewTitle:"Insert Employee"
 })
 });
+
+router.post('/',(req,res)=>{
+   insertRecord(req,res)
+    });
+
+    function insertRecord(req,res)
+{
+var employee=new Employee();
+employee.fullName=req.body.fullName;
+employee.email=req.body.email;
+employee.mobile=req.body.mobile;
+employee.city=req.body.city;
+employee.save((err,doc)=>{
+    if(!err)
+    res.redirect('employee/list');
+    else{
+        if(err.name ==validationError){
+        handelValidationError(err,req.body);
+        res.render('employee/addOrEdit',{
+            viewTitle:"Insert Employee",
+            employee:req.body
+        })
+    }
+
+        else
+        console.log("Error during record insertion :"+err)
+    }
+});
+}
+
+router.get('/list',(req,res)=>{
+    res.json('from list')
+})
+function handelValidationError(err,body){
+    for(field in err.errors){
+        switch(  err.errors[field].path){
+            case 'fullName':
+                body['fullNameError']=err.errors[field].message;
+                break;
+            case 'email':
+                body['emailError']=err.errors[field].message;
+                break;    
+        }
+    }
+  
+
+}
 module.exports =router;
--- models/employee.model.js
+++ models/employee.model.js
@@ -2,7 +2,8 @@ const mongoose =require('mongoose');
 
 var employeeSchema =new mongoose.Schema({
 fullName:{
-    type:String
+    type:String,
+    required:'This Filed is required'
 },
 email:{
     type:String
@@ -14,4 +15,9 @@ city:{
     type:String
 }
 });
+// Custom validation for email
+employeeSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
 mongoose.model('Employee',employeeSchema);
